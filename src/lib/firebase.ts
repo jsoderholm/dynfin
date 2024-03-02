6b9ce03b735b655c007f4ce571575d0ac82c2b1a
jsoderholm <soederholm.john@gmail.com>
2024-03-02 09:45:39 +0100

    Fix Environment Variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA_QJWn5o7-Rd9mddxzOflKCYR8DOOsTn0',
-  authDomain: 'dynfin-b9ddc.firebaseapp.com',
-  projectId: 'dynfin-b9ddc',
-  storageBucket: 'dynfin-b9ddc.appspot.com',
-  messagingSenderId: '91762702683',
-  appId: '1:91762702683:web:d4d49322d9dfe1034e427e',
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
 }
 
 export const app = initializeApp(firebaseConfig)
