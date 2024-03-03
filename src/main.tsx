4ad01af70d969d82bd4c246d4e2b8726e3f41400
Tom Rehnström <64424604+PushingCoder572@users.noreply.github.com>
2024-03-03 16:25:21 +0100

    Implement Tanstack router (#11)
    * Implement Tanstack router
    
    * Fix lock
    
    * Format

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,58 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@/styles/globals.css'
 import '@/styles/index.css'
+import { createRootRoute, Outlet, Link, createRoute, createRouter, RouterProvider } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootRoute = createRootRoute({
+  component: () => (
+    <div className='flex flex-col h-full'>
+      <div className='p-2 flex gap-2'>
+        <Link to='/' className='[&.active]:font-bold'>
+          Home
+        </Link>{' '}
+        <Link to='/about' className='[&.active]:font-bold'>
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  ),
+})
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: function Index() {
+    return <App />
+  },
+})
+
+const aboutRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/about',
+  component: function About() {
+    return <div className='p-2'>Hello from About!</div>
+  },
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, aboutRoute])
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+}
--- src/views/authentication.tsx
+++ src/views/authentication.tsx
@@ -9,7 +9,7 @@ const Authentication = () => {
 
   return (
     <>
-      <div className='container relative hidden h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+      <div className='container relative hidden flex-1 flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
         <UserAuthModal />
         <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
           <div className='absolute inset-0 bg-primary' />
