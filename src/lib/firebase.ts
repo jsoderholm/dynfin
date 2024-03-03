1f96aa4ebf3b55e5c20c1ea0b29d4bcbe4acff71
jsoderholm <103559938+jsoderholm@users.noreply.github.com>
2024-03-03 16:54:43 +0100

    Fix Firebase Emulators (#15)
    Closes #13

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,8 @@
 import { FirebaseApp, initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
-import { Auth, getAuth } from 'firebase/auth'
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
+import { Auth, connectAuthEmulator, getAuth } from 'firebase/auth'
+
+const emulator = import.meta.env.MODE === 'development'
 
 let app: FirebaseApp
 export const initializeFirebase = () => {
@@ -23,9 +25,17 @@ let db: ReturnType<typeof getFirestore>
 
 export const useAuth = () => {
   auth = getAuth(app)
+  if (emulator) {
+    connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true })
+  }
   return auth
 }
 export const useFirestore = () => {
-  if (!db) db = getFirestore(app)
+  if (!db) {
+    db = getFirestore(app)
+    if (emulator) {
+      connectFirestoreEmulator(db, 'localhost', 8080)
+    }
+  }
   return db
 }
