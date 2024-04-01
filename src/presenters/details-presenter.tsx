eee5c37b49a1b803269bdfe76830e88f61527535
jsoderholm <soederholm.john@gmail.com>
2024-04-01 15:13:34 +0200

    Add Details Skeleton

--- src/presenters/details-presenter.tsx
+++ src/presenters/details-presenter.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from '@/components/ui/skeleton'
 import { getCompanyProfileFromFinage } from '@/lib/api/finage'
 import { getCompanyProfileFromFirestore, saveCompanyProfileToFirestore } from '@/lib/db'
 import useDetailsStore from '@/stores/details-store'
@@ -38,14 +39,39 @@ function DetailsPresenter() {
   }, [symbol])
 
   if (!companyProfile) {
-    return <div>Loading...</div>
+    return <Loading />
   }
 
   return (
-    <div className='container'>
-      <CompanyProfileView info={companyProfile} />
-      <NewsListView />
-      <GraphView />
+    <>
+      <div className='container'>
+        <CompanyProfileView info={companyProfile} />
+        <NewsListView />
+        <GraphView />
+      </div>
+    </>
+  )
+}
+
+const Loading = () => {
+  return (
+    <div className='container py-10'>
+      <Skeleton className='mb-6 h-10 w-1/4' />
+      <div className='grid grid-cols-2 gap-10 h-64 mb-10'>
+        {Array.from({ length: 2 }).map((_, i) => (
+          <Skeleton key={i} className='h-64' />
+        ))}
+      </div>
+      <Skeleton className='mb-6 h-10 w-1/4' />
+      <div className='grid grid-cols-4 gap-10 mb-10'>
+        {Array.from({ length: 8 }).map((_, i) => (
+          <Skeleton key={i} className='h-48' />
+        ))}
+      </div>
+      <Skeleton className='mb-6 h-10 w-1/4' />
+      <div>
+        <Skeleton className='h-96' />
+      </div>
     </div>
   )
 }
