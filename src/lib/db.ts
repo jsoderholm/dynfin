ee67bbec37103b511bd9f9e06baa951199bcbad4
jsoderholm <soederholm.john@gmail.com>
2024-04-04 20:18:29 +0200

    Create User in Firestore on Register

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,14 +2,27 @@ import { QueryDocumentSnapshot, collection, doc, getDoc, setDoc } from 'firebase
 import { db } from './firebase'
 import { FirestoreDataConverter } from 'firebase/firestore'
 import { CompanyProfile } from './api/finage'
+import { AuthState } from '@/stores/auth-store'
+import { UserCredential } from 'firebase/auth'
+
+type UserData = {
+  uid: string
+  saved: AuthState['saved']
+}
 
 const companyProfileConverter: FirestoreDataConverter<CompanyProfile> = {
   toFirestore: (data: CompanyProfile) => data,
   fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as CompanyProfile,
 }
 
-const firestore = {
+const userConverter: FirestoreDataConverter<UserData> = {
+  toFirestore: (data: UserData) => data,
+  fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as UserData,
+}
+
+export const firestore = {
   companies: collection(db, 'companies').withConverter(companyProfileConverter),
+  users: collection(db, 'users').withConverter(userConverter),
 }
 
 export async function getCompanyProfileFromFirestore(symbol: string) {
@@ -19,5 +32,10 @@ export async function getCompanyProfileFromFirestore(symbol: string) {
 
 export async function saveCompanyProfileToFirestore(symbol: string, data: CompanyProfile) {
   const docRef = doc(firestore.companies, symbol)
+  await setDoc(docRef, data)
+}
+
+export async function createUserInFirestore(credentials: UserCredential, data: UserData) {
+  const docRef = doc(firestore.users, credentials.user.uid)
   await setDoc(docRef, data)
 }
