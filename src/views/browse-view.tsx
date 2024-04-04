138e40739440706a0a4c811e21aee24098f549b2
jsoderholm <soederholm.john@gmail.com>
2024-04-04 15:39:31 +0200

    Fix Browse View Skeleton
    Closes #39

--- src/views/browse-view.tsx
+++ src/views/browse-view.tsx
@@ -1,5 +1,45 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Link } from '@tanstack/react-router'
+
+const symbols = ['AAPL', 'GOOGL', 'TSLA', 'AMZN', 'MSFT', 'FB']
+
 function BrowseView() {
-  return <div className='container py-10'>Hello world from BrowseView</div>
+  return (
+    <div className='container py-10'>
+      <h2 className='text-3xl font-semibold pb-6'>Browse</h2>
+      <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        {Array.from({ length: 6 }).map((_, i) => (
+          <BrowseItem key={i} i={i} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+interface BrowseItemProps {
+  i: number
+}
+
+const BrowseItem = ({ i }: BrowseItemProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{`News Item ${i + 1}: ${symbols[i]}`}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p className='text-muted-foreground'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.
+        </p>
+      </CardContent>
+      <CardFooter className='flex items-center justify-center'>
+        <Link to='/details/$symbol' params={{ symbol: symbols[i] }}>
+          <Button variant='ghost'>{`View Company Profile for ${symbols[i]}`}</Button>
+        </Link>
+      </CardFooter>
+    </Card>
+  )
 }
 
 export default BrowseView
