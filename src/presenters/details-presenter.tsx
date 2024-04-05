1c885f229783bfce5501a266d26f5bb337969cd2
Tom Rehnström <64424604+PushingCoder572@users.noreply.github.com>
2024-04-05 08:40:48 +0200

    Fix details store symbol (#37)

--- src/presenters/details-presenter.tsx
+++ src/presenters/details-presenter.tsx
@@ -1,6 +1,4 @@
 import { Skeleton } from '@/components/ui/skeleton'
-import { getCompanyProfileFromFinage } from '@/lib/api/finage'
-import { getCompanyProfileFromFirestore, saveCompanyProfileToFirestore } from '@/lib/db'
 import useDetailsStore from '@/stores/details-store'
 import CompanyProfileView from '@/views/company-profile-view'
 import GraphView from '@/views/graph-view'
@@ -19,38 +17,13 @@ function DetailsPresenter() {
     graphInfoLoading: state.graphInfoLoading,
   }))
   const companyProfile = useDetailsStore((state) => state.companyProfile)
+  const setCompanyProfile = useDetailsStore((state) => state.setCompanyProfile)
 
   const loading = newsListLoading || companyProfileLoading || graphInfoLoading
 
   useEffect(() => {
-    /**
-     * 1. Fetch company profile data from Firestore using the symbol.
-     * 1.1. If the data is not found, fetch the data from the Finage API and save it to Firestore.
-     */
-    async function getCompanyProfile() {
-      useDetailsStore.setState({ companyProfileLoading: true })
-      const snapshot = await getCompanyProfileFromFirestore(symbol.toUpperCase())
-
-      if (snapshot.exists()) {
-        useDetailsStore.setState({ companyProfile: snapshot.data(), companyProfileLoading: false })
-        return
-      }
-
-      try {
-        // Fetch the data from the Finage API
-        const data = await getCompanyProfileFromFinage(symbol)
-        // Save the data to Firestore
-        await saveCompanyProfileToFirestore(symbol, data)
-        // Set the data to the store
-        useDetailsStore.setState({ companyProfile: data, companyProfileLoading: false })
-      } catch (e) {
-        useDetailsStore.setState({ companyProfile: null, companyProfileLoading: false })
-        console.error(e)
-      }
-    }
-
-    getCompanyProfile()
-  }, [symbol])
+    setCompanyProfile(symbol)
+  }, [symbol, setCompanyProfile])
 
   if (loading) {
     return <Loading />
