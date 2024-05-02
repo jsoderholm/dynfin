d7c41870fa224475a911d7899cb7bdf962af530f
andas5andas <andas5andas@gmail.com>
2024-05-02 08:40:47 +0200

    Changed to dialog, added badge component and linked the tickers to their details view

--- src/views/browse-view.tsx
+++ src/views/browse-view.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import { IconHeartFilled, IconHeart } from '@tabler/icons-react'
+import { Badge } from '@shadcn/ui'
+import { IconHeartFilled, IconHeart, IconDots } from '@tabler/icons-react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { NewsInfo } from '@/lib/api/stock-news'
 import { Link } from '@tanstack/react-router'
-import { Dialog, DialogTrigger, DialogContent, DialogTitle } from '@/components/ui/dialog'
-
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu'
 interface FavoriteItemProps {
   isFavorited: (ticker: string) => boolean
   onToggleFavorite: (ticker: string, title: string) => void
@@ -47,26 +47,35 @@ const BrowseItem = ({ info, isFavorited, onToggleFavorite }: BrowseItemProps) =>
         <p className='text-muted-foreground'>{text}</p>
       </CardContent>
       <CardFooter className='flex items-center justify-center'>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button variant='ghost'>Show tickers</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogTitle>Tickers in the News</DialogTitle>
-            <table>
-              <tbody>
-                {tickers.map((ticker, index) => (
-                  <tr key={index}>
-                    <td>{ticker}</td>
-                    <td onClick={() => handleToggleFavorite} className='cursor-pointer'>
-                      {isFavorited(ticker) ? <IconHeartFilled /> : <IconHeart />}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </DialogContent>
-        </Dialog>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost'>
+              <IconDots />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem as='div' className='font-bold'>
+              Tickers in the News
+            </DropdownMenuItem>
+            {tickers.map((ticker, index) => (
+              <DropdownMenuItem key={index} className='flex justify-between'>
+                <Link to={`/details/${ticker}`}>
+                  <Badge color='black' variant='outline'>
+                    {ticker}
+                  </Badge>
+                </Link>
+                <span
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    onToggleFavorite(ticker, title)
+                  }}
+                >
+                  {isFavorited(ticker) ? <IconHeartFilled /> : <IconHeart />}
+                </span>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Link to='/details/$symbol' params={{ symbol: tickers[0] }}>
           <Button variant='ghost'>{`View Company Profile for ${tickers[0]}`}</Button>
         </Link>
