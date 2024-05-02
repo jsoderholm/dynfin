3c40701da99967a9356ca47f28b62e20af1a3933
andas5andas <andas5andas@gmail.com>
2024-05-02 11:43:55 +0200

    Added just button and dialog for account deletion

--- src/views/browse-view.tsx
+++ src/views/browse-view.tsx
@@ -28,7 +28,9 @@ const BrowseItem = ({ i }: BrowseItemProps) => {
         <div className='flex-grow'>
           <CardTitle>{`News Item ${i + 1}: ${symbols[i]}`}</CardTitle>
         </div>
-        <Button variant='favorite' className='ml-auto'>Favorite</Button>
+        <Button variant='favorite' className='ml-auto'>
+          Favorite
+        </Button>
       </CardHeader>
       <CardContent>
         <p className='text-muted-foreground'>
