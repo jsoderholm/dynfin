ba3b8fd628b227a23ecea9ff3d6a00834a544fd0
Carl Hansson <carlhansson677@gmail.com>
2024-05-03 09:25:26 +0200

    add loading skeleton

--- src/presenters/browse-presenter.tsx
+++ src/presenters/browse-presenter.tsx
@@ -56,24 +56,16 @@ function BrowsePresenter() {
 
 const Loading = () => {
   return (
-    <div className='container'>
-      <div className='my-6'>
-        <Skeleton className='h-8 w-8' />
-      </div>
-      <Skeleton className='mb-6 h-10 w-1/4' />
-      <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 h-96 mb-10'>
-        <Skeleton className='h-full col-span-1' />
-        <Skeleton className='h-full col-span-2' />
-      </div>
-      <Skeleton className='mb-6 h-10 w-1/4' />
-      <div className='grid grid-cols-4 gap-10 mb-10'>
-        {Array.from({ length: 8 }).map((_, i) => (
-          <Skeleton key={i} className='h-48' />
-        ))}
-      </div>
-      <Skeleton className='mb-6 h-10 w-1/4' />
+    <div className='container my-10'>
       <div>
-        <Skeleton className='h-96' />
+        <div>
+          <Skeleton className='h-10 w-28 rounded' />
+        </div>
+        <div className='my-5 grid gap-6 grid-cols-3'>
+          {Array.from({ length: 6 }).map((_, i) => (
+            <Skeleton key={i} className='h-64' />
+          ))}
+        </div>
       </div>
     </div>
   )
