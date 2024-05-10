4db94bdec0e8e5f9d9d73bc6da5faaa0fb4d5d9e
Carl Hansson <carlhansson677@gmail.com>
2024-05-10 11:33:51 +0200

    clean up browseview

--- src/views/browse-view.tsx
+++ src/views/browse-view.tsx
@@ -6,11 +6,9 @@ import { PaginationNumbers } from '@/components/browse/browse-pagination'
 import MultipleSelector, { Option } from '@/components/ui/multiple-selector'
 import { TOPICS, SECTORS, INDUSTRIES, COUNTRIES, COLLECTIONS } from '@/lib/browse-filtering'
 import { Button } from '@/components/ui/button'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Input } from '@/components/ui/input'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
-import { Label } from '@/components/ui/label'
 import { Filter } from '@/stores/browse-store'
+import { BrowseFilter } from '@/components/browse/browse-filter'
 
 export type PaginationProps = {
   onPageChange: (newPage: number) => void
@@ -100,82 +98,13 @@ function BrowseView(props: BrowseViewProps) {
           <Button className='mr-20' type='submit' variant='outline'>
             Search
           </Button>
-          <Popover defaultOpen={false}>
-            <PopoverTrigger>
-              <Button variant='outline'>Filter</Button>
-            </PopoverTrigger>
-            <PopoverContent className='w-100' onCloseAutoFocus={() => handleSetFilter()}>
-              <div className='flex gap-2'></div>
-              {currentTab === 'all' ? (
-                <div>
-                  <Label>Sectors</Label>
-                  <Select
-                    defaultValue={currentFilter.sector.value}
-                    onValueChange={(value) => (filter.sector = SECTORS.find((sector) => sector.value === value)!)}
-                  >
-                    <SelectTrigger className='min-1/4'>
-                      <SelectValue />
-                    </SelectTrigger>
-
-                    <SelectContent>
-                      {SECTORS.map((sector) => (
-                        <SelectItem value={sector.value}>{sector.label}</SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                  <Label>Industry</Label>
-                  <Select
-                    defaultValue={currentFilter.industry.value}
-                    onValueChange={(value) =>
-                      (filter.industry = INDUSTRIES.find((industry) => industry.value === value)!)
-                    }
-                  >
-                    <SelectTrigger className='min-1/4'>
-                      <SelectValue />
-                    </SelectTrigger>
-
-                    <SelectContent>
-                      {INDUSTRIES.map((industry) => (
-                        <SelectItem value={industry.value}>{industry.label}</SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                  <Label>Country</Label>
-                  <Select
-                    defaultValue={currentFilter.country.value}
-                    onValueChange={(value) => (filter.country = COUNTRIES.find((country) => country.value === value)!)}
-                  >
-                    <SelectTrigger className='min-1/4'>
-                      <SelectValue />
-                    </SelectTrigger>
-
-                    <SelectContent>
-                      {COUNTRIES.map((country) => (
-                        <SelectItem value={country.value}>{country.label}</SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </div>
-              ) : (
-                ''
-              )}
-              <Label>Collection</Label>
-              <Select
-                defaultValue={currentFilter.collection.value}
-                onValueChange={(value) => (filter.collection = COLLECTIONS.find((topic) => topic.value === value)!)}
-              >
-                <SelectTrigger className='min-1/4'>
-                  <SelectValue />
-                </SelectTrigger>
-
-                <SelectContent>
-                  {COLLECTIONS.map((collection) => (
-                    <SelectItem value={collection.value}>{collection.label}</SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </PopoverContent>
-          </Popover>
+          <BrowseFilter
+            filter={filter}
+            currentFilter={currentFilter}
+            setFilter={handleSetFilter}
+            resetFilter={resetFilter}
+            currentTab={currentTab}
+          />
           <TabsList>
             <TabsTrigger value='all'>All News</TabsTrigger>
             <TabsTrigger value='trending'>Trending</TabsTrigger>
@@ -197,9 +126,7 @@ function BrowseView(props: BrowseViewProps) {
             Clear filters
           </Button>
         </div>
-
         <div className='flex pt-3 justify-between gap-3 '></div>
-
         <TabsContent value='all'>
           <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {Array.from(data)
